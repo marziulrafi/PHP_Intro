cbc0dd09522026da32d1271af72c62b83ff5573d
K M Marziul Karim <marziulrafi@gmail.com>
2025-03-04 00:17:24 +0600

    test

---
+++ basic.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basics</title>
+</head>
+
+<body>
+    <h1>Welcome to PHP</h1>
+
+    <?php
+    echo "These are PHP Codes <br>";
+    echo "PHP Keywords are not Case Sensitive. <br>";
+    // Echo echo eChO everything works
+
+    // But variable names are Case Sensitive!
+
+    $color = "Blue";
+    echo "My car is ".$color."<br>";
+    // echo "My house is " . $COLOR . "<br>";
+    // echo "My boat is " . $coLOR . "<br>";
+    ?>
+</body>
+
+</html>
