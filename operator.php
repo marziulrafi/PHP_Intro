1f00da20ae44aaae24784c6010a2b06cb1dbd679
K M Marziul Karim <marziulrafi@gmail.com>
2025-03-05 00:34:08 +0600

    operator

---
+++ operator.php
@@ -0,0 +1,17 @@
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 and $y == 50) {
+    echo "Hellooooooo!!";
+} 
+
+
+$t = date("H");
+if ($t < "2") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
+
